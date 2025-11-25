4e985e63e1cdf85d4a87df31440521998ce4dc4e
JanLageh <jansk123456789@gmail.com>
2025-11-25 15:40:11 +0800

    Revert "Add endpoints and SQL schema for quiz management"
    This reverts commit e985a84f054f962127897248966d110b6474f9cf.

--- OnlineQuiz/IServices/IQuizService.cs
+++ OnlineQuiz/IServices/IQuizService.cs
@@ -8,7 +8,5 @@ namespace OnlineQuiz.IServices
         Task<List<QuizResponseDto>> GetQuizzesForCourseAsync(int courseId, int userId, bool isStudent);
         Task<QuizResponseDto?> GetQuizByIdAsync(int quizId);
         Task<bool> DeleteQuizAsync(int quizId, int userId);
-        Task<QuizResponseDto> UpdateQuizAsync(int quizId, UpdateQuizDto updateQuizDto, int teacherId);
-        Task<QuizResponseDto> PublishQuizAsync(int quizId, bool isPublished, int teacherId);
     }
 }
